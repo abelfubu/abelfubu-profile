a902cb56dff1cc1f0ccda618835fb5fcc2310fe1
abelfubu <abelfubu@gmail.com>
2020-07-01 15:41:39 +0200

    Updates

---
+++ precache-manifest.740d1b5c7b03bc93e902f37b1dffe0a7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062f111469508b08421159247975962f",
+    "url": "/abelfubu/index.html"
+  },
+  {
+    "revision": "e477ad59ff237645349d",
+    "url": "/abelfubu/static/css/main.55d328cb.chunk.css"
+  },
+  {
+    "revision": "9075d8ea0d3ffcb80fde",
+    "url": "/abelfubu/static/js/2.9f0bda04.chunk.js"
+  },
+  {
+    "revision": "5b8b49146ea7ff4abb4f22a9d28772fe",
+    "url": "/abelfubu/static/js/2.9f0bda04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e477ad59ff237645349d",
+    "url": "/abelfubu/static/js/main.6ab5b2ab.chunk.js"
+  },
+  {
+    "revision": "79c52b2f7d20bdc7401e",
+    "url": "/abelfubu/static/js/runtime-main.593ea211.js"
+  },
+  {
+    "revision": "481c6e496d8010e2ebd6d4c11f17ba60",
+    "url": "/abelfubu/static/media/HeroSection.481c6e49.png"
+  },
+  {
+    "revision": "da10860744cc33f9ed3987d5b1976d44",
+    "url": "/abelfubu/static/media/Logo3.da108607.png"
+  },
+  {
+    "revision": "d94bd9f3cad53b23ba71fb23cdaa783b",
+    "url": "/abelfubu/static/media/Montse.d94bd9f3.png"
+  },
+  {
+    "revision": "bd71aac0e5d295f0559197ebb2e906ac",
+    "url": "/abelfubu/static/media/Nums.bd71aac0.png"
+  },
+  {
+    "revision": "a746f0b5a9bb8ce94fa416a6297ebe64",
+    "url": "/abelfubu/static/media/PerfilSm.a746f0b5.jpeg"
+  },
+  {
+    "revision": "5e9f9e9ac3dc9a16548caac0b1c6fcbe",
+    "url": "/abelfubu/static/media/clinica.5e9f9e9a.png"
+  },
+  {
+    "revision": "340d4ddebf7657d8d15ea69ac66ca36b",
+    "url": "/abelfubu/static/media/code.340d4dde.png"
+  },
+  {
+    "revision": "bdbbdd517532799b78a443abe52c1a33",
+    "url": "/abelfubu/static/media/objetivos.bdbbdd51.png"
+  },
+  {
+    "revision": "cf784a052bd51abb17700f642ad58647",
+    "url": "/abelfubu/static/media/prana.cf784a05.png"
+  }
+]);
